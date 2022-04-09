374e43c99bf810360266fe8e75569d9e0350f562
Kim-DaHam <times7749@naver.com>
2022-04-09 18:46:00 +0900

    basic splash xml

--- app/src/main/java/kr/ac/kumoh/lunchpick/Splash.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/Splash.kt
@@ -4,25 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.android.volley.RequestQueue
-import kr.ac.kumoh.lunchpick.contentsList.VolleyRequest
 import kr.ac.kumoh.lunchpick.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
 
-    companion object {
-        const val QUEUE_TAG = "VolleyRequest"
-    }
-
-    private lateinit var mQueue: RequestQueue
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        mQueue = VolleyRequest.getInstance(this.applicationContext).requestQueue
 
         val handler: Handler = Handler()
         handler.postDelayed({
@@ -30,10 +20,5 @@ class Splash : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, 3000)
-    }
-
-    override fun onStop(){
-        super.onStop()
-        mQueue.cancleAll(QUEUE_TAG)
     }
 }
