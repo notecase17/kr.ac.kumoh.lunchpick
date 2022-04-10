a0fe5c8d6ef01ce13b2cf37a0c7eb9751bab0604
Kim-DaHam <times7749@naver.com>
2022-04-10 15:04:33 +0900

    splash & login & signIn & Main Activity UI

--- app/src/main/java/kr/ac/kumoh/lunchpick/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.ac.kumoh.lunchpick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.lunchpick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvSettings.bringToFront()
+        binding.ivSettings.bringToFront()
     }
 }
