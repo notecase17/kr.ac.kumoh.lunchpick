6310e13f63dbae07bdebb2fd803ca2b0270940f1
Kim-DaHam <times7749@naver.com>
2022-04-10 14:16:39 +0900

    login UI without anim

--- app/src/main/java/kr/ac/kumoh/lunchpick/SignUpActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/SignUpActivity.kt
@@ -2,6 +2,9 @@ package kr.ac.kumoh.lunchpick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kr.ac.kumoh.lunchpick.databinding.ActivitySignUpBinding
 
@@ -9,6 +12,9 @@ class SignUpActivity : AppCompatActivity() {
 
     private var isBlank = false
     private var isPwSame = false
+    // 아래 item_gender 불러오는 것에서 nullPointerException 에러 발생
+    //var item_gender = resources.getStringArray(R.array.gender)
+    //var spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, item_gender)
 
     private lateinit var userGender: String
 
@@ -18,13 +24,37 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        /* Spinner 사용 시 코드
+        binding.spGender.adapter = spinnerAdapter
+        binding.spGender.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long)
+            {
+                when (position) {
+                    0 -> {
+                        userGender = "남"
+                    }
+                    1 -> {
+                        userGender = "여"
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+        */
 
+        /* 라디오 버튼 사용 시 코드
         binding.gender.setOnCheckedChangeListener { group, checkId ->
             when(checkId) {
                 R.id.rbMale -> userGender = "남"
                 R.id.rbFemale -> userGender = "여"
             }
         }
+        */
 
         binding.btnSignUp.setOnClickListener {
             val userId = binding.editUserId.text.toString()
