825c3fc0a564d1839743d1ed98d1cec50e90729b
Hojin <notecase17@gmail.com>
2022-05-07 05:56:06 +0900

    Edit app/build.gradle (volley 의존성 추가) Edit  AndroidManifest.kt (http 통신) Edit SignUpActivity.kt (volley 이용한 post 전송. 수정중)

--- app/src/main/java/kr/ac/kumoh/lunchpick/SignUpActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/SignUpActivity.kt
@@ -1,12 +1,21 @@
 package kr.ac.kumoh.lunchpick
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.lunchpick.databinding.ActivitySignUpBinding
+import org.json.JSONException
+import org.json.JSONObject
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -52,8 +61,11 @@ class SignUpActivity : AppCompatActivity() {
             val userName = binding.editName.text.toString()
             val userAge = binding.editAge.text.toString()
 
-            if (signUpCheck(userId, userPw1, userPw2, userName, userAge))
+            if (signUpCheck(userId, userPw1, userPw2, userName, userAge)) {
+                signupVolley(this, url, userId, userPw1, userName, userAge, userGender)
                 Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
@@ -75,6 +87,72 @@ class SignUpActivity : AppCompatActivity() {
             Toast.makeText(this, "닉네임은 한글, 영어, 숫자만 사용 가능 (2~10자)", Toast.LENGTH_SHORT).show()
             return false
         }
+        if (age.toInt()<=0) {
+            Toast.makeText(this, "올바르지 않은 나이입니다.", Toast.LENGTH_SHORT).show()
+        }
         return true
+    }
+
+    private fun signupVolley(context: Context, url: String, id: String, pw: String, name: String, age: String, gender: String) {
+        val requestQueue = Volley.newRequestQueue(context)
+
+        val request: StringRequest = object: StringRequest(Method.POST, url,
+        Response.Listener { response ->
+            showJSONList(response)
+        },
+        Response.ErrorListener { error ->
+            Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show()
+        }) {
+            override fun getParams(): MutableMap<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["userid"] = id
+                params["userpw"] = pw
+                params["username"] = name
+                params["userage"] = age
+                params["usergender"] = gender
+
+                return params
+            }
+        }
+        requestQueue.add(request)
+    }
+
+    fun showJSONList(response: String) {
+        try {
+            Log.e("TAG",response)
+            val jsonObject = JSONObject(response)
+            jsonObject.let {
+                if(it.getString("userinfo") == null) {
+                    showAlert(it.getString("result").toString(), it.getString("message").toString())
+                } else {
+                    if(it.getString("result").toString().equals("success")){
+                        val jsonInfo = JSONObject(it.getString("userinfo").toString())
+                        jsonInfo.let{
+                            // Preference 에 대한 정보 기록은 생략한다.
+                            startActivity(Intent(this,MainActivity::class.java))
+                        }
+                    }
+                }
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+
+        var context: Context? = null
+
+        fun showAlert(title: String, message: String) {
+            val builder = context?.let {
+                AlertDialog.Builder(it)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setCancelable(false)
+                    .setPositiveButton("OK") { dialog, id -> dialog.dismiss() }
+            }
+            val alert = builder!!.create()
+            alert.show()
+        }
     }
 }
