2aed82c7b7d97e185afae6f1f5c6d9e97a31518c
DESKTOP-5P94ADK\김영욱 <dmnion@naver.com>
2022-05-09 23:54:54 +0900

    2022/05/09 Data Binding

--- app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsDetailActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsDetailActivity.kt
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import kr.ac.kumoh.lunchpick.R
 
 class ContentsDetailActivity : AppCompatActivity() {
     lateinit var datas : ContentsModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contents_detail)
 
+        val SERVER_URL = "https://csproject-qejmc.run.goorm.io/"
         val detail_store_image = findViewById<ImageView>(R.id.DetailImage)
         val detail_store_name = findViewById<TextView>(R.id.DetailName)
+        val detail_store_addr = findViewById<TextView>(R.id.DetailAddress)
+        val detail_store_num = findViewById<TextView>(R.id.DetailNum)
 
         datas = intent.getParcelableExtra("data")!!
 
-        Glide.with(this).load(datas.imageUrl).into(detail_store_image)
+        Toast.makeText(this,datas.image, Toast.LENGTH_SHORT).show()
+
+        Glide.with(this).load("${SERVER_URL}images/${datas.image}").into(detail_store_image)
         detail_store_name.text = datas.storeName
+        detail_store_addr.text = datas.storeAddress
+        detail_store_num.text = "가게 번호 : ${datas.storeNum}"
     }
 }
--- app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsRVAdapter.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsRVAdapter.kt
@@ -8,48 +8,69 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.NetworkImageView
 import com.bumptech.glide.Glide
 import kr.ac.kumoh.lunchpick.R
 
-class ContentsRVAdapter(val context: Context, val items : ArrayList<ContentsModel>) :
+class ContentsRVAdapter(
+    val context: Context,
+    private val store: StoreViewModel
+) :
     RecyclerView.Adapter<ContentsRVAdapter.ViewHolder>() {
+
+    /*interface ItemClick{
+        fun onClick(view : View, position: Int)
+    }
+    var itemClick : ItemClick? = null*/
 
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ContentsRVAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.content_rv_item, parent, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ContentsRVAdapter.ViewHolder, position: Int) {
-        holder.bindItems(items[position])
+        /*if(itemClick != null){
+            holder.itemView.setOnClickListener { v->
+                itemClick?.onClick(v, position)
+            }
+        }*/
+        holder.bindItems(store.getStore(position))
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return store.getSize()
     }
+
+
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
+        val SERVER_URL = "https://csproject-qejmc.run.goorm.io/"
+
         fun bindItems(item : ContentsModel){
-            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+            val imageViewArea = itemView.findViewById<NetworkImageView>(R.id.imageArea)
             val Name = itemView.findViewById<TextView>(R.id.storeName)
             val Address = itemView.findViewById<TextView>(R.id.storeAddress)
             /*context란 이 액티비티에서 사용하는 맥락(?)이다
             * load(item.imageUrl을 imageViewArea에 집어 넣겠다*/
 
-            Glide.with(context)
+            /*Glide.with(context)
                 .load(item.imageUrl)
-                .into(imageViewArea)
+                .into(imageViewArea)*/
+            imageViewArea.setImageUrl("${SERVER_URL}images/${item.image}"
+                ,store.mImageLoader)
             Name.text = item.storeName
             Address.text = item.storeAddress
 
             //각 리스트 클릭시 이벤트
             itemView.setOnClickListener {
-                //putExtra와 Parcelize로 item clss를 넘겨준다.
                 Intent(context, ContentsDetailActivity::class.java).apply{
+                    //putExtra와 Parcelize로 item clss를 넘겨준다.
                     putExtra("data", item)
                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 }.run { context.startActivity(this)}
