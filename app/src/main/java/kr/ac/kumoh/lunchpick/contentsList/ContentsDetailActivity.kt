a3c410ca2fbdf6b3d3957135c17623f267d672fc
DESKTOP-5P94ADK\김영욱 <dmnion@naver.com>
2022-05-16 03:56:21 +0900

    2022/05/16 다함님이 주신 레이아웃 파일 연동

--- app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsDetailActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/ContentsDetailActivity.kt
@@ -42,6 +42,7 @@ class ContentsDetailActivity : AppCompatActivity() {
         val SERVER_URL = "https://csproject-qejmc.run.goorm.io/"
         val detail_store_image = findViewById<ImageView>(R.id.DetailImage)
         val detail_store_name = findViewById<TextView>(R.id.DetailName)
+        val detail_store_name2 = findViewById<TextView>(R.id.DetailName2)
         val detail_store_addr = findViewById<TextView>(R.id.DetailAddress)
         val detail_store_num = findViewById<TextView>(R.id.DetailNum)
 
@@ -52,6 +53,7 @@ class ContentsDetailActivity : AppCompatActivity() {
 
         Glide.with(this).load("${SERVER_URL}images/${datas.image}").into(detail_store_image)
         detail_store_name.text = datas.storeName
+        detail_store_name2.text = datas.storeName
         detail_store_addr.text = datas.storeAddress
         detail_store_num.text = "가게 번호 : ${datas.storeNum}"
     }
--- app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/MenusRVAdapter.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/contentsList/MenusRVAdapter.kt
@@ -19,7 +19,7 @@ class MenusRVAdapter(
         viewType: Int
     ): MenusRVAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.content_rv_item, parent, false)
+            .inflate(R.layout.menu_rv_item, parent, false)
         return ViewHolder(v)
     }
 
@@ -35,8 +35,8 @@ class MenusRVAdapter(
         val SERVER_URL = "https://csproject-qejmc.run.goorm.io/"
 
         fun bindItems(item : MenusModel){
-            val imageViewArea = itemView.findViewById<NetworkImageView>(R.id.imageArea)
-            val Name = itemView.findViewById<TextView>(R.id.storeName)
+            val imageViewArea = itemView.findViewById<NetworkImageView>(R.id.iv_FoodImg)
+            val Name = itemView.findViewById<TextView>(R.id.tv_foodName)
             /*context란 이 액티비티에서 사용하는 맥락(?)이다
             * load(item.imageUrl을 imageViewArea에 집어 넣겠다*/
 
