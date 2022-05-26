9a7d7c197931437f4c26450054f267edbc10698f
DESKTOP-장정익 <zmzf1@naver.com>
2022-05-26 12:44:34 +0900

    게임진행

--- app/src/main/java/kr/ac/kumoh/lunchpick/GameActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/GameActivity.kt
@@ -4,55 +4,95 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import kr.ac.kumoh.lunchpick.databinding.ActivityGameBinding
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.Serializable
 
 data class food (
-    var name: String
+    val name: String,
+    val ima : String
 ) : Serializable
 
 class GameActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityGameBinding
+    companion object{
+        const val QUEUE_TAG = "VolleyRequest"
+    }
+    private lateinit var binding: ActivityGameBinding
+    lateinit var mqueue: RequestQueue
+    var mResult: JSONObject? = null
+    var mArray = ArrayList<food>()
+
+    override fun  onStop(){
+        super.onStop()
+        mqueue.cancelAll(QUEUE_TAG)
+    }
+    fun requestMenu() {
+        val url = "https://csproject-qejmc.run.goorm.io/Menu"
+        val request = JsonObjectRequest(
+            Request.Method.GET,
+            url, null,
+            { response ->
+                mResult = response
+                makesel() },
+            { error ->
+                Toast.makeText(this,error.toString(),Toast.LENGTH_LONG).show()
+            })
+
+        request.tag = QUEUE_TAG
+        mqueue.add(request)
+    }
+
+    fun makesel() {
+        val items: JSONArray = mResult?.getJSONArray("menu_ID") ?: return
+        mArray.clear()
+        for (i in 0 until items!!.length()){
+            val item = items[i] as JSONObject
+            val name = item.getString("menu")
+            val ima = item.getString("image")
+            mArray.add(food(name,ima))
+            idnt[i] = mArray[i]
+        }
+    }
+    val SERVER_URL = "https://csproject-qejmc.run.goorm.io/"
     var idnt = Array<food?>(32) {null}
     var count = 0
     var c_count = 0
     var round = 0
     var n_round = 1
+    var size = 8
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mqueue = Volley.newRequestQueue(this)
+        requestMenu()
 
-        val fd1 = food("초밥")
-        val fd2 = food("떡볶이")
-        val fd3 = food("라면")
-        val fd4 = food("김치찌개")
-        val fd5 = food("파스타")
-        val fd6 = food("치킨")
-        val fd7 = food("짜장면")
-        val fd8 = food("피자")
-        val fd = arrayOf<food>(fd1,fd2,fd3,fd4,fd5,fd6,fd7,fd8)
-        c_count = fd.size
+        c_count = size
+        round = size
 
-        for(i in fd.indices){
-            idnt[i]=fd[i]
-        }
-
-        round = fd.size
-
-        binding.showImage.text = idnt[count++]!!.name
-        binding.showImage2.text = idnt[count++]!!.name
+        Glide.with(this).load("${SERVER_URL}image/${idnt[count]!!.ima}").into(binding.FirstImage)
+        binding.FItext.text = idnt[count++]!!.name
+        Glide.with(this).load("${SERVER_URL}image/${idnt[count]!!.ima}").into(binding.secondImage)
+        binding.SItext.text = idnt[count++]!!.name
         binding.progressing.text = round.toString() + "강 " + "( "+n_round.toString()+" / "+(round/2).toString()+" )"
 
-        binding.showImage.setOnClickListener {
-            Toast.makeText(this, binding.showImage.text.toString(), Toast.LENGTH_SHORT).show()
-            val fd = food(binding.showImage.text.toString())
+        binding.FirstImage.setOnClickListener {
+            // Toast.makeText(this, binding.FItext.text.toString(), Toast.LENGTH_SHORT).show()
+            val fd = idnt[count-2]
             idnt[c_count++]=fd
             click()
         }
-        binding.showImage2.setOnClickListener {
-            Toast.makeText(this, binding.showImage.text.toString(), Toast.LENGTH_SHORT).show()
-            val fd = food(binding.showImage.text.toString())
+        binding.secondImage.setOnClickListener {
+            //Toast.makeText(this, binding.SItext.text.toString(), Toast.LENGTH_SHORT).show()
+            val fd = idnt[count-1]
             idnt[c_count++]=fd
             click()
         }
@@ -71,8 +111,10 @@ class GameActivity : AppCompatActivity() {
             finish()
         }
         else{
-            binding.showImage.text = idnt[count++]!!.name
-            binding.showImage2.text = idnt[count++]!!.name
+            Glide.with(this).load("${SERVER_URL}image/${idnt[count]!!.ima}").into(binding.FirstImage)
+            binding.FItext.text = idnt[count++]!!.name
+            Glide.with(this).load("${SERVER_URL}image/${idnt[count]!!.ima}").into(binding.secondImage)
+            binding.SItext.text = idnt[count++]!!.name
             when (round) {
                 4 -> {
                     binding.progressing.text = "준결승 " + "( "+n_round.toString()+" / "+(round/2).toString()+" )"
@@ -86,6 +128,4 @@ class GameActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }
--- app/src/main/java/kr/ac/kumoh/lunchpick/WinnerActivity.kt
+++ app/src/main/java/kr/ac/kumoh/lunchpick/WinnerActivity.kt
@@ -3,10 +3,12 @@ package kr.ac.kumoh.lunchpick
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kr.ac.kumoh.lunchpick.databinding.ActivityWinnerBinding
 
 class WinnerActivity : AppCompatActivity() {
     private lateinit var binding:ActivityWinnerBinding
+    val url = "https://csproject-qejmc.run.goorm.io/StoreMenu"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +17,8 @@ class WinnerActivity : AppCompatActivity() {
         val intent: Intent = intent
         val fd :food = intent.getSerializableExtra("winner") as food
 
-        binding.winnerFoodName.text = "우    \"" + fd.name + "\"    승"
-        binding.winnerFood.text = fd.name
+        binding.winnerFoodName.text = "우    \"" + fd!!.name + "\"    승"
+        Glide.with(this).load("${url}images/${fd!!.ima}").into(binding.WFimage)
+        binding.WFText.text = fd!!.name
     }
-
 }
